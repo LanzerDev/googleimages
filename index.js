8a616f5f4883fa204bcbfd44ecdffeeec3c9d379
lanzerDev <hdsnpej@gmail.com>
2024-09-10 10:46:16 -0600

    deploy with fly io

--- index.js
+++ index.js
@@ -8,36 +8,28 @@ app.set('json spaces', 2)
 
 app.get('/search', (req, res) => {
     const search = new SerpApi.GoogleSearch("eb0003d328469f1459a392a58424ffdf2be3feea1fec1f2f23517275740b3367");
-    const busqueda = req.query.q + "-pelicula";
+    const busqueda = req.query.movie_name + " Poster Pelicula";
 
-    console.log("se realizo la busqueda: " + busqueda)
+    console.log("Se realizo la busqueda: " + busqueda)
     const params = {
         engine: "google_images",
         q: busqueda
     };
 
     search.json(params, (data) => {
-
-        let busquedaSugerida;
-        let imagenSrc;
-
-        for (let i in data.suggested_searches) {
-            if (data.suggested_searches[i].name == "poster" || data.suggested_searches[i].name == "movie" || data.suggested_searches[i].name == "wallpaper") { 
-                busquedaSugerida = data.suggested_searches[i].name;
-                imagenSrc = data.suggested_searches[i].thumbnail
-            }
-
-            if(busquedaSugerida == ""){
-                busquedaSugerida = data.suggested_searches[0].name + " (busqueda por defecto)";
-                imagenSrc = data.suggested_searches[0].thumbnail;
-            }
-        }
-
-        console.log("devolvimos la busqueda sugerida: " + busquedaSugerida)
-        console.log("imagen que devolvemos: " + imagenSrc)
-        res.json(imagenSrc)
+        console.log("Respuesta de la api", data)
+        res.json(searchTheBestPoster(data))
+        //res.json(data)
     });
 });
+
+
+
+
+
+function searchTheBestPoster(data){
+    return data.images_results.find(elemento => elemento.title.includes('Poster'))
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
